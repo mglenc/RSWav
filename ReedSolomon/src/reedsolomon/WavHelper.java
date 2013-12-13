426bd03d8b88265a4a9d7d404d0f1eac08a79665
Michał Glenc <mglenc@gmail.com>
2013-12-13 15:43:10 +0100

    Dekoder dla bledow w czesci kontrolnej

--- ReedSolomon/src/reedsolomon/WavHelper.java
+++ ReedSolomon/src/reedsolomon/WavHelper.java
@@ -59,10 +59,18 @@ public class WavHelper {
                 for(int i = 0; i < 64; i++) {
                     infoInt[i] = infoBitSet.get(i) ? 1 : 0;
                 }
-              
                 
                 //tu wywołanie kodera i dekodera zwracającego tablice byte'ów do odtworzenia
-                coder(infoInt);
+                byte[] coderData = new byte[5];
+                coderData[0] = 64;
+                coderData[1] = 120;
+                coderData[2] = 70;
+                coderData[3] = 15;
+                coderData[4] = 1;
+                
+                int[] codedData = coder(infoInt, coderData);
+                
+                int[] decodedData = decoder(codedData, coderData);
             }
         }
         catch(IOException ex){
@@ -72,16 +80,38 @@ public class WavHelper {
         return 1;
     }
     
-    public int coder(int[] data) {
-        Mathematic math = new Mathematic();
+    public int hammingWeight(int data[]) {
+        int count = 0;
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] == 1) count++;
+        }
         
-        byte[] coderData = new byte[5];
-        coderData[0] = 64;
-        coderData[1] = 120;
-        coderData[2] = 70;
-        coderData[3] = 15;
-        coderData[4] = 1;
-                
+        return count;
+    }
+    
+    public int[] rotateRight( int data[], int k) {
+            int temp = data[data.length-1];
+            for(int i = 0; i < k; i++) {
+                System.arraycopy(data, 0, data, 1, data.length-1);
+                data[0] = temp;
+            }
+        
+        return data;
+    }
+    
+    public int[] rotateLeft(int data[], int k) {
+        int temp = data[0];
+        for(int i = 0; i < k; i++) {
+            System.arraycopy(data, 1, data, 0, data.length-1);
+            data[data.length-1] = temp;
+        }
+        
+        return data;
+    }
+    
+    Mathematic math = new Mathematic();
+    
+    public int[] coder(int[] data, byte[] coderData) {
         BitSet codePolyBitSet = BitSet.valueOf(coderData);
         int[] codePolyInt = new int[40];
         
@@ -126,20 +156,89 @@ public class WavHelper {
         
         //Zaklamanie jednego bitu
         //dataMultiplied[7] = 1;
+        dataMultiplied[25] = 1;
+        //dataMultiplied[32] = 1;
         
-        //Obliczanie reszty z dzielenia zakodowanych danych przez weilomian generujacy
-        result = math.divideVectors(dataMultiplied, codePolyInt);
-        resultVector = result.getKey();
-        restVector = result.getValue();
+        return dataMultiplied;
+    }
+    
+    public int[] decoder(int[] data, byte[] coderData) {
+        BitSet codePolyBitSet = BitSet.valueOf(coderData);
+        int[] codePolyInt = new int[40];
         
-        System.out.printf("\nReszta z dzielenia zakodowanego słowa:");
+        int correctingAbility = 2;
+        
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 8; j++) {
+                codePolyInt[8 * i + j] = codePolyBitSet.get(8 * i + j) ? 1 : 0;
+            }
+        }
+        
+        System.out.printf("\nWielomian generujacy:");
+        for(int i = 0; i < codePolyInt.length; i++) {
+            System.out.printf(Integer.toString(codePolyInt[i]));
+        }
+
+        //data = rotateRight(data, 8);
+        
+        //Obliczanie syndromu
+        AbstractMap.SimpleEntry<int [], int []> result = math.divideVectors(data, codePolyInt);
+        int[] resultVector = result.getKey();
+        int[] restVector = result.getValue();
+        
+        System.out.printf("\nReszta z dzielenia zakodowanego słowa (syndrom błędu):");
         for(int i = 0; i < restVector.length; i++) {
             System.out.printf(Integer.toString(restVector[i]));
         }
+
+        int[] decodedData = new int[64];
+        //Obliczanie wagi bledu i porownanie ze zdolnoscia korekcyjna t
+        if(hammingWeight(restVector) <= correctingAbility) {
+            //Bledy w czesci kontrolnej, kopiujemy dobre dane
+            System.arraycopy(data, 32, decodedData, 0, decodedData.length);
+        } else {
+            boolean readyForCorrection = false;
+            int rotateCount = 0;
+            int dataRotated[] = data;
+            //Przesuniecie cykliczne w lewo
+            while(!readyForCorrection) {
+                rotateCount++;
+                dataRotated = rotateLeft(dataRotated, 1);
+                
+                System.out.printf("\n%d. Obrot danych w lewo: ", rotateCount);
+                for(int i = 0; i < dataRotated.length; i++) {
+                    System.out.printf(Integer.toString(dataRotated[i]));
+                }
+                
+                result = math.divideVectors(dataRotated, codePolyInt);
+                resultVector = result.getKey();
+                restVector = result.getValue();
+                
+                System.out.printf("\nSyndrom: ");
+                for(int i = 0; i < restVector.length; i++) {
+                    System.out.printf(Integer.toString(restVector[i]));
+                }
+                
+                int weight = hammingWeight(restVector);
+                
+                System.out.printf("\nWaga Hamminga syndromu: %d", weight);
+                
+                System.out.printf("\n");
+                
+                if(weight <= correctingAbility) {
+                    readyForCorrection = true;
+                }
+            }
+        }
         
-        System.out.printf("\n");
+        System.out.printf("\nDane zdekodowane: ");
+        for(int i = 0; i < decodedData.length; i++) {
+            System.out.printf(Integer.toString(decodedData[i]));
+        }
         
-        return 0;
+        System.out.printf("\n\n");
+        
+        return decodedData;
     }
 
 }
